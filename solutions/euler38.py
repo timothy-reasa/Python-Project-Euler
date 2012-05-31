8cc62499fd6952ce0caf2b317e153163984272fe
Timothy Reasa <timothy.reasa@gmail.com>
2012-05-31 12:42:23 -0500

    Initial commit for euler38.py

---
+++ solutions/euler38.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+###############################################################################
+#
+# Project Euler Problem 38
+# found online at projecteuler.net/problem=38
+# Solution by Timothy Reasa
+#
+###############################################################################
+
+limit = 1000000
+description = \
+'Take the number 192 and multiply it by each of 1, 2, and 3:\n\n' + \
+'\t192  1 = 192\n' + \
+'\t192  2 = 384\n' + \
+'\t192  3 = 576\n\n' + \
+'By concatenating each product we get the 1 to 9 pandigital,\n' + \
+'192384576. We will call 192384576 the concatenated product of 192\n' + \
+'and (1,2,3).\n\n' + \
+'The same can be achieved by starting with 9 and multiplying by 1, 2,\n' + \
+'3, 4, and 5, giving the pandigital, 918273645, which is the\n' + \
+'concatenated product of 9 and (1,2,3,4,5).\n\n' + \
+'What is the largest 1 to 9 pandigital 9-digit number that can be\n' + \
+'formed as the concatenated product of an integer with (1,2, ... , n)\n' + \
+'where n  1?\n'
+
+def display(self):
+    return description
+
+def isPandigital9(n):
+    n = str(n)
+    if len(n) != 9:
+	return False
+    for i in range(1,10):
+	if not str(i) in n:
+	    return False
+    return True
+
+def solve(self):
+    maxPan = 0
+    i = 1
+    while len(str(i) + str(2*i)) < 10:	# must be product of at least 2 mults
+        temp = ''
+	j = 1
+	while len(temp) < 9:
+	    temp += str(j*i)
+	    j += 1
+        if temp > maxPan and isPandigital9(temp):
+	    maxPan = temp
+  	i += 1
+ 
+    return maxPan
+
+
+###############################################################################
+# 
+# If executed as a script/not imported
+#
+###############################################################################
+if __name__ == '__main__':
+    print solve(None)
