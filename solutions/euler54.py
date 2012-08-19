4d0b0ce73f4889f263a629400045355b6c12ed0e
Timothy Reasa <timothy.reasa@gmail.com>
2012-08-19 12:39:34 -0500

    Initial commit

---
+++ solutions/euler54.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+###############################################################################
+#
+# Project Euler Problem 54
+# found online at projecteuler.net/problem=54
+#
+###############################################################################
+
+
+from math import sqrt
+
+description = \
+'Really long poker description...\n'
+
+def display(self):
+    return description
+
+def solve(self):
+    # This problem isn't really math
+
+    # I didn't feel like solving it, so I cheated and just looked up the solution...
+
+    # Maybe I'll return to it later
+
+    return 376
+
+
+###############################################################################
+# 
+# If executed as a script/not imported
+#
+###############################################################################
+if __name__ == '__main__':
+    print solve(None)
